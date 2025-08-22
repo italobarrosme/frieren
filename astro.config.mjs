f7aabed6d10c54e64c506ebe64b5bb0a6aa36719
italobarrosme <italobarros.me@gmail.com>
2025-08-21 21:31:52 -0300

    chore: update astro.config.mjs to use node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,11 @@ import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
 import react from '@astrojs/react';
 
-import vercel from '@astrojs/vercel/serverless';
+// import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
   output: 'server', // Configurar para SSR
-  adapter: vercel(), // Adaptador Node.js para SSR
+  // adapter: vercel(), // Adaptador Node.js para SSR
+  adapter: node({ mode: 'standalone' }),
   integrations: [react()], // Integración de Preact
 });
